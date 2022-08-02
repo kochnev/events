9e5a4c3cebab27502ae69535ce8479b9451c5b41
Alexander <lyotchik1@mail.ru>
2022-08-02 20:56:44 +0400

    add event_detail page

--- events/afisha/urls.py
+++ events/afisha/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    ]
+    path("events/<event_id>/", views.event_detail, name='event_detail'),
+
+]
--- events/afisha/views.py
+++ events/afisha/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Event
 
 
 def index(request):
     events = Event.objects.all().order_by('date_from')
     return render(request, 'afisha/index.html', {'events': events})
+
+def event_detail(request, event_id):
+    e = get_object_or_404(Event, id=event_id)
+    return render(request, 'afisha/event_detail.html', {'event': e })
