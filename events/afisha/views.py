9daa802695091395d25baef0c3d864eb7efa30dd
Alexander <lyotchik1@mail.ru>
2022-11-20 23:10:26 +0400

    add descending ordering by time inside one date

--- events/afisha/views.py
+++ events/afisha/views.py
@@ -4,11 +4,10 @@ from .models import Event
 
 
 def index(request):
-    events = Event.objects.filter(event_date__gte=timezone.now()).order_by('event_date')
-    #events = Event.objects.all().order_by('date_from')
+    events = Event.objects.filter(event_date__gte=timezone.now()).order_by('event_date', '-time_from')
     return render(request, 'afisha/index.html', {'events': events})
 
 
 def event_detail(request, event_id):
     e = get_object_or_404(Event, id=event_id)
-    return render(request, 'afisha/event_detail.html', {'event': e })
+    return render(request, 'afisha/event_detail.html', {'event': e})
